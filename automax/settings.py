e9cf55a6aa0bb8e1b59cbb740326046ab653ce90
Ree Jay Anigan <real.anigan@gmail.com>
2025-07-01 22:00:00 +0800

    added new model

--- automax/settings.py
+++ automax/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'localflavor',
     'main',  # Custom app for the project
     'users',  # Custom app for user management
 ]
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Location
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    pass
